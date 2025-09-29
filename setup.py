3d6f0dfecb4e6c157060e214c0f121ab65e015d6
Nikhil Chouhan <61838991+Nickkss@users.noreply.github.com>
2025-09-29 17:01:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='production_dashboard',
+    version='1.0.0',
+    description='Custom Job Card Production Dashboard for ERPNext',
+    author='Nickkss',
+    author_email='your-email@example.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[]
+)

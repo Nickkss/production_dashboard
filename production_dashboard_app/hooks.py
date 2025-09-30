1e867c04b6402feeff5e0c019cbd1f301515ce25
Nikhil Chouhan <61838991+Nickkss@users.noreply.github.com>
2025-09-30 10:38:49 +0530

    Update hooks.py

--- production_dashboard_app/hooks.py
+++ production_dashboard_app/hooks.py
@@ -1,1 +1,10 @@
+app_name = "production_dashboard"
+app_title = "Production Dashboard"
+app_publisher = "Nickkss"
+app_description = "Custom Job Card Production Dashboard for ERPNext"
+app_email = "your-email@example.com"
+app_license = "MIT"
 
+doctype_js = {
+    "Job Card": "production_dashboard_app/job_card_dashboard.js"
+}
